6ff5590fcf1231fa4417ab7dc41b55502a5a9fd0
Frikkie3006 <21100292@virtualwindow.co.za>
2023-08-09 14:33:38 +0200

    Splash Screen,Icon implementation & front end of the first "home view" screen

---
+++ app/src/main/java/com/example/moelas/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.moelas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this,landingScreen::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
+    }
+}
---
+++ app/src/main/java/com/example/moelas/landingScreen.kt
@@ -0,0 +1,11 @@
+package com.example.moelas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class landingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_screen)
+    }
+}
