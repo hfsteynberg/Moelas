281434ff77f4ad41c79ab8064a16fd43e4a5054f
Frikkie3006 <21100292@virtualwindow.co.za>
2023-08-09 19:01:03 +0200

    Early stage of implementation of category screen.

---
+++ app/src/main/java/com/example/moelas/categoryScreen.kt
@@ -0,0 +1,11 @@
+package com.example.moelas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class categoryScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_screen)
+    }
+}
