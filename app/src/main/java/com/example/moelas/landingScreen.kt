64bb5459bf8447855fdce1d943950fb76ff17cbc
Frikkie3006 <21100292@virtualwindow.co.za>
2023-08-09 20:29:54 +0200

    Basicimplementation of category screen + Navigation

--- app/src/main/java/com/example/moelas/landingScreen.kt
+++ app/src/main/java/com/example/moelas/landingScreen.kt
@@ -1,11 +1,20 @@
 package com.example.moelas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class landingScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_screen)
+
+        val categoryScreenNav = findViewById<Button>(R.id.button)
+        categoryScreenNav.setOnClickListener{
+            val Intent = Intent(this,categoryScreen::class.java)
+            startActivity(Intent)
+        }
+
     }
 }
